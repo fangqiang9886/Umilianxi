0417fe1c42b9e5ee031d9a0b927bb8dd87099938
fangqiang <fangqiang9886@163.com>
2020-07-07 16:44:01 +0800

    1次

---
+++ .umirc.js
@@ -0,0 +1,35 @@
+
+// ref: https://umijs.org/config/
+export default {
+  treeShaking: true,
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/index',
+      routes: [
+        { path: '/', redirect: '/user' },
+        { path: '/user', component: './users/index' }
+      ]
+    }
+  ],
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: false,
+      title: 'Umilianxi',
+      dll: true,
+      
+      routes: {
+        exclude: [
+          /models\//,
+          /services\//,
+          /model\.(t|j)sx?$/,
+          /service\.(t|j)sx?$/,
+          /components\//,
+        ],
+      },
+    }],
+  ],
+}
